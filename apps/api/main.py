876f5bf431eae904577bcdea65119971fdee115b
tanishapritha <155418018+tanishapritha@users.noreply.github.com>
2026-02-21 02:34:44 +0530

    feat(api): implement api endpoints

---
+++ apps/api/main.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+# Route POST /users/login/access-token
+
+# Route GET /users/me
+
+# Route GET /frameworks/
+
+# Route GET /{policy_id}/report
+
+# Route GET /{policy_id}/pdf
+
+# Route GET /{policy_id}/status
+
+# Route POST /upload
+
+# Route POST /
+
+# Route GET /
